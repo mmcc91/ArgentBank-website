99cfe1b2dc05e50e31803d15b413784cdc1e452a
mmcc91 <121430505+mmcc91@users.noreply.github.com>
2024-05-23 17:46:02 +0200

    visuel de la page utilisateur en cours

--- front-end/src/components/account/account.jsx
+++ front-end/src/components/account/account.jsx
@@ -1,24 +1,20 @@
-import { PropTypes } from "prop-types";
-import Button from "../button/button";
+
 import "./account.scss";
 
-const Account = ({title,amount,description}) => {
+const Account = ({ title, type, accountnumber, amount, description }) => {
   return (
     <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">{title}</h3>
-          <p className="account-amount">{amount}</p>
-          <p className="account-amount-description">{description}</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <Button className={"transaction-button"} btnText={"View transactions"}></Button>
-        </div>
-      </section>
+      <h6>Titre provisoire pour être sûr</h6>
+      <div className="account-content-wrapper">
+        <h3 className="account-title">{title} {type} {accountnumber}</h3>
+        <p className="account-amount">{amount}</p>
+        <p className="account-amount-description">{description}</p>
+      </div>
+      <div className="account-content-wrapper cta">
+        <button className="transaction-button">View transactions</button>
+      </div>
+    </section>
   );
 };
-Account.propTypes = {
-  title: PropTypes.string.isRequired,
-  amount: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-};
+
 export default Account;
--- front-end/src/pages/user.jsx
+++ front-end/src/pages/user.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import Footer from "../components/footer/footer";
 import Header from "../components/header/header";
-
+import Account from "../components/account/account";
+import accountsData from "../Data/dataAccount.json";
 
 
 const User = () => {
   return (
     <div>
       <Header />
-
-      <h1>ecrire ici la page de l'user quand il est logger </h1>
-     
+      <main className="main bg-dark">
+        {accountsData.map((account) => (
+          <Account
+            key={account.id}
+            title={account.title}
+            type={account.type}
+            accountnumber={account.accountnumber}
+            amount={account.amount}
+            description={account.description}
+          />
+        ))}
+      </main>
       <Footer />
     </div>
   );
 };
+
 export default User;
