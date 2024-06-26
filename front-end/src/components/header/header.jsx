c080c63af81bf9e61c50ab27371f38a4127aa381
mmcc91 <121430505+mmcc91@users.noreply.github.com>
2024-06-26 11:16:02 +0200

    refactor img png jpeg (delete) to webp

--- front-end/src/components/header/header.jsx
+++ front-end/src/components/header/header.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate, Link } from "react-router-dom"; // Mise à jour pour react-router-dom@6
 
-import Logo from "../../../designs/img/argentBankLogo.png";
+import Logo from "../../../designs/img/argentBankLogo.webp";
 import "./header.scss";
 import { updateUsername, cleanStore, setUserInfo } from '../../redux/slices/userSlice';
 
--- front-end/src/components/navBar/navBar.jsx
+++ front-end/src/components/navBar/navBar.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
-import logo from '../../../designs/img/argentBankLogo.png';
+import logo from '../../../designs/img/argentBankLogo.webp';
 import  {logout } from '../../redux/slices/auth.js';
 import './navBar.scss';
 
